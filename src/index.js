b5959e92b91757f1262cdf6361f966a2af143dbb
buoyantair <buoyantair@protonmail.com>
2019-03-09 10:54:52 +0530

    Add Queries, Resolvers etc

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 import { config } from 'dotenv'
 import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import cors from 'cors'
+import { ApolloServer, gql } from 'apollo-server-express'
 
 config()
 const app = express()
-const schema = null
-const resolvers = null
+app.use(cors())
+const schema = gql`
+  type Query {
+    me: User
+    user(id: ID!): User
+    users: [User!]
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`
+
+let users = {
+  1: {
+    id: '1',
+    username: 'buoyantair'
+  },
+  2: {
+    id: '2',
+    username: 'Vasili Záitsev'
+  }
+}
+
+const resolvers = {
+  Query: {
+    me: (parent, args, { me }) => me,
+    user: (parent, { id }) => users[id],
+    users: () => Object.values(users)
+  },
+  User: {
+    username: user => user.username
+  }
+}
 
 const server = new ApolloServer({
   typeDefs: schema,
-  resolvers
+  resolvers,
+  context: {
+    me: users[1]
+  }
 })
 
 server.applyMiddleware({
