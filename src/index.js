c7b2fe7add3b7a5c59753441bae7c7cea76e7a49
buoyantair <buoyantair@protonmail.com>
2019-03-09 11:30:45 +0530

    Add first mutation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { config } from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import { ApolloServer, gql } from 'apollo-server-express'
+import uuid4 from 'uuid/v4'
 
 config()
 const app = express()
@@ -11,22 +12,50 @@ const schema = gql`
     me: User
     user(id: ID!): User
     users: [User!]
+    messages: [Message!]!
+    message(id: ID!): Message!
+  }
+
+  type Mutation {
+    createMessage(text: String!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]!
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `
 
 let users = {
   1: {
     id: '1',
-    username: 'buoyantair'
+    username: 'buoyantair',
+    messageIds: [1]
   },
   2: {
     id: '2',
-    username: 'Vasili Záitsev'
+    username: 'Vasili Záitsev',
+    messageIds: [2]
+  }
+}
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1'
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2'
   }
 }
 
@@ -34,10 +63,28 @@ const resolvers = {
   Query: {
     me: (parent, args, { me }) => me,
     user: (parent, { id }) => users[id],
-    users: () => Object.values(users)
+    users: () => Object.values(users),
+    message: (parent, { id }) => messages[id],
+    messages: () => Object.values(messages)
+  },
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuid4()
+      const message = {
+        text,
+        userId: me.id,
+        id
+      }
+      messages[id] = message
+      users[me.id].messageIds.push(id)
+      return message
+    }
+  },
+  Message: {
+    user: ({ userId }, args) => users[userId]
   },
   User: {
-    username: user => user.username
+    messages: user => Object.values(messages).filter(m => m.userId === user.id)
   }
 }
 
